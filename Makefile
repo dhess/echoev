8400211904f7b6dc1aae5eaa6bf223407a39760f
Drew Hess <dhess-src@bothan.net>
2011-04-18 15:28:09 -0700

    Initial commit.
    echoev isn't an echo server yet. It simply accepts connections on the
    wildcard IPv4 and IPv6 addresses on port 7777, then closes them, and
    writes a log to stdout.

---
+++ Makefile
@@ -0,0 +1,30 @@
+PLATFORM=-D ECHOEV_PLATFORM_MACOSX
+#PLATFORM=-D ECHOEV_PLATFORM_LINUX
+#PLATFORM=-D ECHOEV_PLATFORM_BSD
+
+CC=gcc
+INCLUDES=-I /usr/local/include
+CFLAGS=-O2 -g $(INCLUDES) $(PLATFORM)
+
+LD=gcc
+LIBS=-L /usr/local/lib
+LDFLAGS=$(LIBS)
+
+all:	echoev
+
+help:
+	@echo "Targets:"
+	@echo
+	@echo "all - build echoev binary."
+	@echo "help - this message."
+
+echoev: echoev.o
+	$(LD) -o echoev $(LDFLAGS) -lev $<
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $<
+
+clean:
+	rm -f echoev *.o
+
+.PHONY:	clean
---
+++ echoev.c
@@ -0,0 +1,198 @@
+/*
+ * echoev.c
+ * A simple echo server, implemented with libev.
+ *
+ * Copyright (c) 2011 Drew Hess <dhess-src@bothan.net>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <ctype.h>
+#include <ev.h>
+
+static void log_msg(const char *msg)
+{
+    puts(msg);
+}
+
+static void log_err(const char *errmsg)
+{
+    perror(errmsg);
+}
+
+static void log_notice(const char *msg)
+{
+    log_msg(msg);
+}
+
+static void log_debug(const char *msg)
+{
+    log_msg(msg);
+}
+
+static void log_connection(const struct sockaddr_storage *addr,
+                           socklen_t addr_len)
+{
+    char ip[INET6_ADDRSTRLEN];
+    const void *src;
+    if (addr->ss_family == AF_INET)
+        src = &((const struct sockaddr_in *) addr)->sin_addr;
+    else
+        src = &((const struct sockaddr_in6 *) addr)->sin6_addr;
+    log_notice(inet_ntop(addr->ss_family, src, ip, INET6_ADDRSTRLEN));
+}
+
+static void listen_cb(EV_P_ ev_io *w, int revents)
+{
+    log_debug("listen_cb called");
+
+    /*
+     * libev recommends calling accept() in a loop for best
+     * performance when using the select or poll back ends. The ev_io
+     * watcher's file descriptor had better be non-blocking!
+     */
+    while (true) {
+        struct sockaddr_storage addr;
+        socklen_t addr_len = sizeof(addr);
+
+        int fd = accept(w->fd, (struct sockaddr *) &addr, &addr_len);
+        if (fd == -1) {
+            if (errno != EWOULDBLOCK)
+                log_err("accept");
+            break;
+        }
+
+        log_connection(&addr, addr_len);
+        log_notice("closing connection");
+        close(fd);
+    }
+}
+
+/*
+ * Create, bind, and listen on a non-blocking socket using the given
+ * socket address.
+ *
+ * Return the socket's file descriptor, or -1 if an error occured, in
+ * which case the error is logged, the error code is left in errno,
+ * and -1 is returned.
+ */
+int listen_on(const struct sockaddr *addr, socklen_t addr_len)
+{
+    int fd = socket(addr->sa_family, SOCK_STREAM, 0);
+    if (fd == -1) {
+        log_err("socket");
+        return -1;
+    }
+    if (fcntl(fd, F_SETFL, O_NONBLOCK) == -1) {
+        log_err("fcntl");
+        goto err;
+    }
+    if (bind(fd, addr, addr_len) == -1) {
+        log_err("bind");
+        goto err;
+    }
+    if (listen(fd, 0) == -1) {
+        log_err("listen");
+        goto err;
+    }
+    return fd;
+
+  err:
+    close(fd);
+    return -1;
+}
+
+ev_io *make_listener(const struct sockaddr *addr, socklen_t addr_len)
+{
+    int listen_fd = listen_on(addr, addr_len);
+    if (listen_fd == -1)
+        return NULL;
+
+    ev_io *watcher = malloc(sizeof(ev_io));
+    if (watcher)
+        ev_io_init(watcher, listen_cb, listen_fd, EV_READ);
+    return watcher;
+}
+
+
+                     
+const uint16_t port = 7777;
+
+int main(int argc, char *argv[])
+{
+    struct ev_loop *loop = EV_DEFAULT;
+
+    /*
+     * Regarding IPv4 and IPv6 wildcard binds on the same port:
+     *
+     * The Linux kernel maps both IPv4 and IPv6 wildcard binds to the
+     * same local port space, in which case only one family can be
+     * bound to a given port, and the IPv6 wildcard bind will see both
+     * IPv4 and IPv6 traffic. BSD-based platforms (e.g., Mac OS X)
+     * recommend listening on two sockets for the same port, one for
+     * IPv4 and one for IPv6, when you want to accept traffic for both
+     * transports, especially when access control (firewalling) is in
+     * effect.
+     */
+
+    struct sockaddr_in6 saddr6;
+    memset(&saddr6, 0, sizeof(saddr6));
+    saddr6.sin6_family = AF_INET6;
+    saddr6.sin6_addr = in6addr_any;
+    saddr6.sin6_port = htons(port);
+
+    ev_io *listen6_watcher = make_listener((const struct sockaddr *)&saddr6,
+                                           sizeof(saddr6));
+    if (listen6_watcher)
+        ev_io_start(loop, listen6_watcher);
+    else
+        exit(errno);
+
+#ifndef ECHOEV_PLATFORM_LINUX
+    struct sockaddr_in saddr;
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+
+    ev_io *listen_watcher = make_listener((const struct sockaddr *)&saddr,
+                                          sizeof(saddr));
+    if (listen_watcher)
+        ev_io_start(loop, listen_watcher);
+    else
+        exit(errno);
+#endif
+    
+    ev_run(loop, 0);
+    
+    return 0;
+}
